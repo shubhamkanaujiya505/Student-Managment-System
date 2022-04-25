bb5a8163c4225cff93e57d0ff571babad5497ee1
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2022-04-25 10:24:50 +0530

    Initial commit Adding user input

---
+++ StudentManagmentSystem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // user input
+    int value; // store input
+    while (true)  //for infinite loop 
+    {
+        cout<<"Press 1 to enter data"<<endl;
+        cout<<"Press 2 to show data"<<endl;
+        cout<<"Press 3 to search data"<<endl;
+        cout<<"Press 4 to update data"<<endl;
+        cout<<"Press 5 to delete data"<<endl;
+        cout<<"Press 6 to exit "<<endl;
+        cin>>value;
+
+    }
+    
+    return 0;
+}
